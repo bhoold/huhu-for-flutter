6370bafcae458ef5b1bf5f294bdd37c7bd0866e4
Raven <bhoold@yahoo.com>
2019-02-20 22:28:07 +0800

    项目初始化

---
+++ lib/home.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        leading: IconButton(
+          tooltip: 'menu',
+          icon: Icon(Icons.menu),
+          onPressed: () {
+
+          },
+        ),
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            tooltip: 'search',
+            icon: Icon(Icons.search),
+            onPressed: () {
+
+            },
+          ),
+          IconButton(
+            tooltip: 'more',
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+
+            },
+          )
+        ],
+        flexibleSpace: Icon(Icons.mic),
+      ),
+      body: new ContactPage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class ContactPage extends StatelessWidget {
+  _buildContactList() {
+    return <ContactModal>[
+      const ContactModal(
+          fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+      const ContactModal(
+          fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+      const ContactModal(
+          fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+      const ContactModal(
+          fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+      const ContactModal(
+          fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+      const ContactModal(
+          fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+      const ContactModal(
+          fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+      const ContactModal(
+          fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+      const ContactModal(
+          fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(body: new ContactsList(_buildContactList()));
+  }
+}
+
+
+
+class ContactsList extends StatelessWidget {
+  final List<ContactModal> _contactModal;
+
+  ContactsList(this._contactModal);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView(
+      padding: new EdgeInsets.symmetric(vertical: 8.0),
+      children: _buildContactsList(),
+    );
+  }
+
+  List<ContactListItem> _buildContactsList() {
+    return _contactModal
+        .map((contact) => new ContactListItem(contact))
+        .toList();
+  }
+}
+
+class ContactListItem extends StatelessWidget {
+  final ContactModal _contactModal;
+
+  ContactListItem(this._contactModal);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+        leading: new CircleAvatar(child: new Text(_contactModal.fullName[0])),
+        title: new Text(_contactModal.fullName),
+        subtitle: new Text(_contactModal.email));
+  }
+}
+
+class ContactModal {
+  final String fullName;
+  final String email;
+
+  const ContactModal({this.fullName, this.email});
+}
